00a3e88eb26cf1e0d6db096d4944f5a26b20e385
Suraj Ravichandran <suraj@ixsystems.com>
2016-12-01 15:30:42 -0800

    this if is no longer needed.
    Ticket: #18866

--- pySMART/device.py
+++ pySMART/device.py
@@ -1038,7 +1038,7 @@ class Device(object):
                         if 'power on time' in line:
                             self.diags['Power_On_Hours'] = line.split(':')[1].split(' ')[1]
         # map temperature
-        if self.temperature is None and self.attributes[194]:
+        if self.temperature is None:
             # in this case the disk is probably ata
             try:
                 self.temperature = int(self.attributes[194].raw)
