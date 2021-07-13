truenas/py-SMART

commit 5c43f7ad3316651fe9336ff87af3f8239290b306

Testing github+pypi workflow 1

pySMART/__init__.py
@@ -101,13 +101,12 @@ Having recently migrated my development from Batch to Python for Linux
 portabiity, I thought a simple wrapper for smartctl would save time in the
 development of future automated test tools.
 """
+from .testentry import TestEntry
+from .device_list import DeviceList
+from .device import Device, smart_health_assement
+from .attribute import Attribute
 from . import utils
 utils.configure_trace_logging()
 
-from .attribute import Attribute
-from .device import Device, smart_health_assement
-from .device_list import DeviceList
-from .testentry import TestEntry
 
-
-__version__ = '0.3'
+__version__ = '1.0.1'

.github/workflows/publish-to-test-pypi.yml
@@ -1,0 +1,36 @@
+name: Publish Python 🐍 distributions 📦 to PyPI and TestPyPI
+on: push
+jobs:
+  build-n-publish:
+    name: Build and publish Python 🐍 distributions 📦 to PyPI and TestPyPI
+    runs-on: ubuntu-18.04
+    steps:
+    - uses: actions/checkout@master
+    - name: Set up Python 3.7
+      uses: actions/setup-python@v1
+      with:
+        python-version: 3.7
+    - name: Install pypa/build
+      run: >-
+        python -m
+        pip install
+        build
+        --user
+    - name: Build a binary wheel and a source tarball
+      run: >-
+        python -m
+        build
+        --sdist
+        --wheel
+        --outdir dist/
+        .
+    - name: Publish distribution 📦 to Test PyPI
+      uses: pypa/gh-action-pypi-publish@master
+      with:
+        password: ${{ secrets.TEST_PYPI_API_TOKEN }}
+        repository_url: https://test.pypi.org/legacy/
+    - name: Publish distribution 📦 to PyPI
+      if: startsWith(github.ref, 'refs/tags')
+      uses: pypa/gh-action-pypi-publish@master
+      with:
+        password: ${{ secrets.PYPI_API_TOKEN }}

.gitignore
@@ -1,4 +1,5 @@
 ﻿venv
 pySMART.egg-info
 .idea
+.pypirc
 *.pyc

setup.py
@@ -43,7 +43,7 @@ setup(
     author='Marc Herndon',
     author_email='Herndon.MarcT@gmail.com',
     packages=['pySMART'],
-    url='none',
+    url='https://github.com/Naudit/py-SMART',
     license='GNU LGPLv2.1.html',
     description='Wrapper for smartctl (smartmontools)',
     long_description=open('README.md').read(),

commit ef3f086f7eec0adee2746d0d9705abcb340c560e

Load device after trace logger as been configured
as device.py loads and uses the logger with the trace method, which does not exist until configure_trace_logging() has been called, hence produces:
```
AttributeError: 'Logger' object has no attribute 'trace'
```

Regression introduced with: https://github.com/truenas/py-SMART/commit/5c43f7ad3316651fe9336ff87af3f8239290b306

Signed-off-by: MichaIng <micha@dietpi.com>

pySMART/__init__.py
@@ -102,11 +102,11 @@ portabiity, I thought a simple wrapper for smartctl would save time in the
 development of future automated test tools.
 """
 from .testentry import TestEntry
-from .device_list import DeviceList
-from .device import Device, smart_health_assement
 from .attribute import Attribute
 from . import utils
 utils.configure_trace_logging()
+from .device_list import DeviceList
+from .device import Device, smart_health_assement
 
 
 __version__ = '1.0.5'