9577d820ed157eb793d683822d9f4adc4b508e3f
Rafael Leira <rafael.leira@naudit.es>
2021-07-20 12:37:35 +0200

    include support for python 3.10 on setup.py

--- setup.py
+++ setup.py
@@ -71,6 +71,7 @@ CLASSIFIERS = [
     'Programming Language :: Python :: 3.7',
     'Programming Language :: Python :: 3.8',
     'Programming Language :: Python :: 3.9',
+    'Programming Language :: Python :: 3.10',
     'Topic :: Software Development',
     'Topic :: Software Development :: Libraries',
     'Topic :: Software Development :: Libraries :: Python Modules'
