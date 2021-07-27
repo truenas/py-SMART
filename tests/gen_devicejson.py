34d1b7e8f3dc375484e3f0afa4419a71261d934f
Rafael Leira <rafael.leira@naudit.es>
2021-07-27 10:58:56 +0200

    added tool to generate device.json test info

---
+++ tests/gen_devicejson.py
@@ -0,0 +1,100 @@
+# Copyright (C) 2021 Rafael Leira, Naudit HPCN S.L.
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License,
+# version 2, as published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+# MA  02110-1301, USA.
+#
+################################################################
+
+import argparse
+import json
+import os
+import sys
+
+from pySMART import Device
+
+from .smartctlfile import SmartctlFile
+
+
+def main():
+
+    parser = argparse.ArgumentParser(
+        description='Generate device.json from data stored in file for future tests.')
+    parser.add_argument('--folder', required=True,
+                        help='The folder where the device info is stored')
+    parser.add_argument('--device', required=True,
+                        help='The device name')
+    parser.add_argument('--interface', default=None,
+                        help='The device interface')
+
+    args = parser.parse_args()
+
+    folder = args.folder
+    device_name = args.device
+    interface_name = args.interface
+
+    sf = SmartctlFile(folder)
+
+    json_dict = {"name": device_name}
+
+    if interface_name is None:
+        dev = Device(device_name, smartctl=sf)
+
+    else:
+        dev = Device(device_name, interface=interface_name, smartctl=sf)
+        json_dict['interface'] = interface_name
+
+    json_dict['tests'] = vars(dev)
+
+    # Remove non serializable objects
+    to_delete = [
+        'smartctl',
+    ]
+
+    # add to list private objects
+    for entry in json_dict['tests']:
+        if entry[0] == '_':
+            to_delete.append(entry)
+
+    for todel in to_delete:
+        if todel in json_dict['tests']:
+            del json_dict['tests'][todel]
+
+    # Transform attributes
+    if 'attributes' in json_dict['tests']:
+        att_list = []
+        for att in json_dict['tests']['attributes']:
+            if att is None:
+                att_list.append(None)
+            else:
+                att_list.append(vars(att))
+
+        json_dict['tests']['attributes'] = att_list
+
+    # Transform tests
+    if 'tests' in json_dict['tests']:
+        test_list = []
+        for tst in json_dict['tests']['tests']:
+            if tst is None:
+                test_list.append(None)
+            else:
+                test_list.append(vars(tst))
+
+        json_dict['tests']['tests'] = test_list
+
+    with open(os.path.join(folder, 'device.json'), "w") as f:
+        f.write(json.dumps(json_dict, indent=4))
+
+
+if __name__ == "__main__":
+    main()
